63970d74ec4c63484d4f3f46eb545e21bd2b2d6e
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-05-12 04:41:38 +0300

    Created a connection to MongoDB. Corrected controllerWrapper

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+const mongoose = require('mongoose');
+// require('dotenv').config();
 const { app } = require('./app');
+const { DB_HOST, PORT = 3000 } = process.env;
 
-// app.listen(3333, () => {
-//   console.log('Server running. Use our API on port: 3333');
-// });
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(
-    `Server running. Use our API on port: 3000  http://localhost:${PORT}`
-  );
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log(
+        `Server running. Use our API on port: ${PORT}  http://localhost:${PORT}`
+      );
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
