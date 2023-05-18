c83ad9d7ea03491260343097fec393bd27c0bcf9
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-05-18 06:38:43 +0300

    Added the ability to upload a user avatar
    Created a public/avatars folder.
    Added avatarURL ( user schema).
    Added package gravatar.
    Created an endpoint /users/avatars (PATCH method).
    Sizes set to 250x250 (jimp package).

--- routes/api/contactsRoutes.js
+++ routes/api/contactsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { validateBody, isValidId, autenticate } = require('../../middlewares');
+const { validateBody, isValidId, auth } = require('../../middlewares');
 const schemas = require('../../schemas');
 const {
   getContacts,
@@ -14,25 +14,15 @@ const router = express.Router();
 
 router
   .route('/')
-  .get(autenticate, getContacts)
-  .post(autenticate, validateBody(schemas.addSchemaContact), addContact);
+  .get(auth, getContacts)
+  .post(auth, validateBody(schemas.addSchemaContact), addContact);
 
 router
   .route('/:contactId')
-  .get(autenticate, isValidId, getContactById)
-  .delete(autenticate, isValidId, removeContact)
-  .put(
-    autenticate,
-    isValidId,
-    validateBody(schemas.addSchemaContact),
-    updateContact
-  );
+  .get(auth, isValidId, getContactById)
+  .delete(auth, isValidId, removeContact)
+  .put(auth, isValidId, validateBody(schemas.addSchemaContact), updateContact);
 
-router.patch(
-  '/:contactId/favorite',
-  autenticate,
-  isValidId,
-  updateStatusContact
-);
+router.patch('/:contactId/favorite', auth, isValidId, updateStatusContact);
 
 module.exports = router;
